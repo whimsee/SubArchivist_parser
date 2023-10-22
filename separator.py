c2c8957099103027b0785688437a164e04e0e2fe
whimsee <jbtenmatay@gmail.com>
2023-10-22 01:47:04 -0700

    updated separator

--- separator.py
+++ separator.py
@@ -7,22 +7,15 @@ ed_lyrics = []
 with open("test.ass", "r") as file:
     while True:
         next_line = file.readline()
-        print(next_line)
+#         print(next_line)
         
         if not next_line:
             break
         
         if "Original Script" in next_line:
             this_line = next_line.split(": ")[1].split("  [")[0]
-            text_extract.update({"Original Script" : this_line})
+            text_extract.update({"Original_Script" : this_line})
         
-        if next_line == "Format: Layer,Start,End,Style,Name,MarginL,MarginR,MarginV,Effect,Text\n":
-            print("NEW LOOP")
-            break
-    
-
-    while True:
-        next_line = file.readline()
 #         next_line.split(",")
 #         print(next_line)
         
@@ -30,22 +23,22 @@ with open("test.ass", "r") as file:
             break
         
         if "OP LYRICS" in next_line:
-            print("OP LYRICS")
             op_lyrics.append(next_line.split(",", 9)[9].strip("\n"))
             
         if "ED LYRICS" in next_line:
-            print("ED LYRICS")
             ed_lyrics.append(next_line.split(",", 9)[9].strip("\n"))
         
-        next_line.strip()
+#         next_line.strip()
 
 # print(this_line)
-# print(text_extract)
+
 print(op_lyrics)
 print(ed_lyrics)
 
-op_lyrics_full = "\n".join(op_lyrics)
-ed_lyrics_full = "\n".join(ed_lyrics)
-print(ftfy.fix_encoding(op_lyrics_full))
+op_lyrics_full = ftfy.fix_encoding("\n".join(op_lyrics))
+ed_lyrics_full = ftfy.fix_encoding("\n".join(ed_lyrics))
+print(op_lyrics_full)
 print("---------")
-print(ftfy.fix_encoding(ed_lyrics_full))
+print(ed_lyrics_full)
+text_extract.update({"ed_lyrics" : op_lyrics_full})
+print(text_extract)
