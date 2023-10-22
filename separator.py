bf677d19e4edd8e632297b311c42bb822d8006b1
whimsee <jbtenmatay@gmail.com>
2023-10-22 01:28:00 -0700

    separator

--- separator.py
+++ separator.py
@@ -1,7 +1,56 @@
+import ftfy
+
+text_extract = {}
+op_lyrics = []
+ed_lyrics = []
+
+
 with open("test.ass", "r") as file:
+#     file.decode("utf-8").encode("windows-1252")
     while True:
         next_line = file.readline()
-
+        print(next_line)
+        
         if not next_line:
-            break;
-        print(next_line.strip())
+            break
+        
+        if "Original Script" in next_line:
+            this_line = next_line.split(": ")[1].split("  [")[0]
+            text_extract.update({"Original Script" : this_line})
+        
+        if next_line == "Format: Layer,Start,End,Style,Name,MarginL,MarginR,MarginV,Effect,Text\n":
+            print("NEW LOOP")
+            break
+    
+
+    while True:
+        next_line = file.readline()
+#         next_line.split(",")
+#         print(next_line)
+        
+        if not next_line:
+            break
+        
+        if "OP LYRICS" in next_line:
+            print("OP LYRICS")
+#             print(next_line.split(",")[9])
+            op_lyrics.append(next_line.split(",")[9].strip("\n"))
+            
+        if "ED LYRICS" in next_line:
+            print("ED LYRICS")
+#             print(next_line.split(",")[9])
+            this_line = next_line.split(",")[9].strip("\n")
+            ed_lyrics.append(this_line)
+        
+        next_line.strip()
+
+# print(this_line)
+# print(text_extract)
+print(op_lyrics)
+print(ed_lyrics)
+
+op_lyrics_full = "\n".join(op_lyrics)
+ed_lyrics_full = "\n".join(ed_lyrics)
+print(ftfy.fix_encoding(op_lyrics_full))
+print("---------")
+print(ftfy.fix_encoding(ed_lyrics_full))
