f1d52820020f98046dd792100b950ee58bb8975c
Gylfie <jbtenmatay@gmail.com>
2023-10-23 22:50:27 -0700

    First pass done

--- aggregate_links.py
+++ aggregate_links.py
@@ -3,15 +3,15 @@ import subprocess
 
 with open("links.json", 'r', encoding="utf8") as file:
     data = json.load(file)
-    print(data['title'], data['season'])
+    title = data['title']
+    season = data['season']
     
     for x, y in data['episodes'].items():    
         print(x, y)
+        subprocess.run("curl -o subs/" + title + "/" + "/" + season + "/" + x + ".ass $(crunchy-cli search --audio ja-JP -o '{{subtitle.locale}} {{subtitle.url}}' " + y + " | grep 'en-US' | awk '{print $2}')", shell=True)
 
-
-links = ("https://www.crunchyroll.com/watch/G6K5K9Z7Y/kotoura-san-and-manabe-kun","https://www.crunchyroll.com/watch/GR49GVW86/but-youre-not-here")
 
 # for link in links:
 # 	print (subprocess.check_output("curl $(crunchy-cli search --audio ja-JP -o '{{subtitle.locale}} {{subtitle.url}}' " + link + " | grep 'en-US' | awk '{print $2}')", shell=True, text=True))
-subprocess.run("curl -o subs/test.ass $(crunchy-cli search --audio ja-JP -o '{{subtitle.locale}} {{subtitle.url}}' https://www.crunchyroll.com/watch/GRDKJZ81Y/alone-and-lonesome | grep 'en-US' | awk '{print $2}')", shell=True)
+
 
