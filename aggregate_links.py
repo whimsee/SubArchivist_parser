3a73ff576b3ba67771dbc74b9520637be2115ecc
Gylfie <jbtenmatay@gmail.com>
2023-11-07 11:02:41 -0800

    Separated folder title from link title in curl

--- aggregate_links.py
+++ aggregate_links.py
@@ -5,7 +5,9 @@ import shutil
 
 with open("links.json", 'r', encoding="utf8") as file:
     data = json.load(file)
-    title = data['title'].replace(" ","_").replace("'","\\'").replace("/", "//").replace(";", "_").replace(":", "")
+#     title = data['title'].replace(" ","_").replace("'","\\'").replace("/", "//").replace(";", "_").replace(":", "")
+    title = data['title'].replace(" ","_").replace("/", "//").replace(";", "_").replace(":", "")
+    link_title = data['title'].replace(" ","_").replace("'","\\'").replace("/", "//").replace(";", "_").replace(":", "")
     season = data['season'].replace(" ","_")
     
     Path("subs/" + title + "/" + season).mkdir(parents=True, exist_ok=True)
@@ -13,7 +15,7 @@ with open("links.json", 'r', encoding="utf8") as file:
     for x, y in data['episodes'].items():    
         print(x, y)
         x = x.replace(" ","_").replace("'","\\'").replace(":", "-").replace("?","").replace("(", "_").replace(")","_")
-        subprocess.run("curl -o subs/" + title + "/" + "/" + season + "/" + x + ".ass $(crunchy-cli search --audio ja-JP -o '{{subtitle.locale}} {{subtitle.url}}' " + y + " | grep 'en-US' | awk '{print $2}')", shell=True)
+        subprocess.run("curl -o subs/" + link_title + "/" + season + "/" + x + ".ass $(crunchy-cli search --audio ja-JP -o '{{subtitle.locale}} {{subtitle.url}}' " + y + " | grep 'en-US' | awk '{print $2}')", shell=True)
 #     subprocess.run("curl -o subs/E9_-_What\\'s_done_is_done.ass $(crunchy-cli search --audio ja-JP -o '{{subtitle.locale}} {{subtitle.url}}' https://www.crunchyroll.com/watch/GD9UVJ9JN/whats-done-is-done | grep 'en-US' | awk '{print $2}')", shell=True)
     try:
         with open("subs/" + title + "/" + season + "/" + "name_dict.json", 'x') as f:
