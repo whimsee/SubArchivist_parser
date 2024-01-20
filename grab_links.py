20de412a25bb01f5b309422874a7e487e9313dc5
whimsee <jbtenmatay@gmail.com>
2024-01-20 00:46:22 -0800

    pretty print JSON

--- grab_links.py
+++ grab_links.py
@@ -58,7 +58,7 @@ except FileExistsError:
 
 # Generate links.json for processing
 with open("subs/" + folder_title + "/" + folder_season + "/" + "links.json", "w", encoding="utf8") as file:
-    json.dump(data, file,  ensure_ascii=False, indent=4)
+    json.dump(data, file,  ensure_ascii=False, indent="\t", separators=(',', ' : '))
     
     
     
