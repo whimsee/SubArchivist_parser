5009621aef4628ba957568e5bd01408f0f636b57
Jerico <jbtenmatay@gmail.com>
2024-03-20 22:59:28 -0700

    added secrets example

---
+++ secrets_example.py
@@ -0,0 +1,7 @@
+secrets = {
+    "book_url" : "http://web/api/books/",
+    "chapter_url" : "http://web/api/chapters/",
+    "page_url" : "http://web/api/pages/",
+    
+    "API_ID_TOKEN" : "tokenID:tokenSecret"
+    }
