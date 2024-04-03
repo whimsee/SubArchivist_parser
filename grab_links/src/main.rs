455861f4a4b4e2035061d2c835358d3375cf081d
whimsee <whimsee>
2024-04-03 14:02:14 -0700

    rust updates

--- grab_links/src/main.rs
+++ grab_links/src/main.rs
@@ -1,28 +1,14 @@
-use crunchyroll_rs::{Crunchyroll, MediaCollection};
-use crunchyroll_rs::parse::UrlType;
+use std::env;
+use std::fs;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Log in to Crunchyroll with your email and password.
-    // Support for username login was dropped by Crunchyroll on December 6th, 2023
-    let crunchyroll = Crunchyroll::builder()
-        .login_with_credentials("<email>", "<password>")
-        .await?;
+fn main() {
+    let file_path = "src/test.txt";
+    // --snip--
+    println!("In file {}", file_path);
 
-    let url = crunchyroll_rs::parse_url("https://www.crunchyroll.com/watch/GRDQPM1ZY/alone-and-lonesome").expect("url is not valid");
-    if let UrlType::EpisodeOrMovie(media_id) = url {
-        if let MediaCollection::Episode(episode) = crunchyroll.media_collection_from_id(media_id).await? {
-            println!(
-                "Url is episode {} ({}) of {} season {}",
-                episode.episode_number,
-                episode.title,
-                episode.series_title,
-                episode.season_number
-            )
-        }
-    } else {
-        panic!("Url is not a episode")
-    }
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
 
-    Ok(())
+    println!("With text:\n{contents}");
+    println!("{}",contents);
 }
