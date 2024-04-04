5e677255128662d14384b36cb481714a2f7b39b9
whimsee <whimsee>
2024-04-04 10:31:18 -0700

    rust updates

--- grab_links/src/main.rs
+++ grab_links/src/main.rs
@@ -1,8 +1,65 @@
-use std::env;
 use std::fs;
 use anyhow::Result;
 use crunchyroll_rs::common::StreamExt;
 use crunchyroll_rs::Crunchyroll;
+use std::fmt;
+// use crunchyroll_rs::media::Subtitle;
+// use crunchyroll::Locale;
+
+pub enum Locale {
+    ar_ME,
+    ar_SA,
+    ca_ES,
+    de_DE,
+    en_IN,
+    en_US,
+    es_419,
+    es_ES,
+    es_LA,
+    fr_FR,
+    hi_IN,
+    id_ID,
+    it_IT,
+    ja_JP,
+    ko_KR,
+    ms_MY,
+    pl_PL,
+    pt_BR,
+    pt_PT,
+    ru_RU,
+    ta_IN,
+    te_IN,
+    th_TH,
+    tr_TR,
+    vi_VN,
+    zh_CN,
+    zh_HK,
+    zh_TW,
+    Custom(String),
+}
+
+impl fmt::Display for Locale {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct Subtitle {
+    pub locale: Locale,
+    pub url: String,
+    pub format: String,
+    /* private fields */
+}
+
+impl Summary for Subtitle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.locale.to_string(), self.url, self.format)
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
